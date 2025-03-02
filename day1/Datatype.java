b27f184d4e6542d2cdfd7919047b6f2429c74983
monz200 <silvyanevergiveup24@gmail.com>
2025-03-02 22:33:16 +0530

    Add files via upload
    day1 assign

---
+++ day1/Datatype.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		
+		int truckNo=1825478;
+		boolean isATruck=true;
+		float speed=254.547892f;
+		char logo='A';
+		
+        String name="Hero Honda";
+		
+		long phoneNo=9587462145l;
+		double capacity=124.67874565;
+		 
+		String type="3267.624.578.2472";
+		
+		System.out.println(type);		
+		System.out.println(capacity);		  
+		System.out.println(phoneNo);
+		 
+		System.out.println("Vehicle No:- "+truckNo+"\n"+"Name of the Vehicle Brand:- "+name+"\n"+"Is the vehicle is truck? "+isATruck+"\n"+"Logo of the Vehicle:- "+logo+"\n"+"Speed of the Vehicle:- "+speed);
+		 
+
+	}
+
+}
---
+++ day1/FirstProg.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class FirstProg {
+
+	public static void main(String[] args) {
+		System.out.println("welcom to testleaf");
+
+	}
+
+}
